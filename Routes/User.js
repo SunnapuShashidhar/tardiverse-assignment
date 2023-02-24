aaf1cf9e5a59b2035f8dc1c0436afd3d52743837
Shashidhar Sunnapu <86150741+SunnapuShashidhar@users.noreply.github.com>
2023-02-24 20:22:13 +0530

    Update User.js

--- Routes/User.js
+++ Routes/User.js
@@ -4,10 +4,10 @@ var {SignIn,SignUp,getUser,requiresSignIn,deleteUser,updateUser}=require("../Con
 router.post("/sign-up",SignUp);
 router.post("/sign-in",SignIn);
 //curd operations
-// router.get("/user",requiresSignIn,getUser)
-// router.put("/update",requiresSignIn,updateUser);
-// router.delete("/delete",requiresSignIn,deleteUser)
-router.get("/user",getUser)
-router.put("/update",updateUser);
-router.delete("/delete",deleteUser)
+router.get("/user",requiresSignIn,getUser)
+router.put("/update",requiresSignIn,updateUser);
+router.delete("/delete",requiresSignIn,deleteUser)
+// router.get("/user",getUser)
+// router.put("/update",updateUser);
+// router.delete("/delete",deleteUser)
 module.exports=router;
